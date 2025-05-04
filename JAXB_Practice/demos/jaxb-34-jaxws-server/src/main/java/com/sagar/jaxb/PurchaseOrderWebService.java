c807d398b724ac4411d38301a40b63b2735d0327
sagarrao1 <sagarrao1@gmail.com>
2025-05-04 11:44:48 +0530

    upgraded from jdk 8 to jdk 11

--- JAXB_Practice/demos/jaxb-34-jaxws-server/src/main/java/com/sagar/jaxb/PurchaseOrderWebService.java
+++ JAXB_Practice/demos/jaxb-34-jaxws-server/src/main/java/com/sagar/jaxb/PurchaseOrderWebService.java
@@ -1,5 +1,6 @@
 package com.sagar.jaxb;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -12,6 +13,11 @@ import javax.jws.WebResult;
 import javax.jws.WebService;
 import javax.xml.ws.Endpoint;
 
+import com.sagar.jaxb.domain.Address;
+import com.sagar.jaxb.domain.Customer;
+import com.sagar.jaxb.domain.Item;
+import com.sagar.jaxb.domain.Loyalty;
+//import com.sagar.jaxb.domain.Loyalty;
 import com.sagar.jaxb.domain.PurchaseOrder;
 
 
@@ -28,7 +34,7 @@ private final AtomicLong ID_GENERATOR = new AtomicLong();
 @WebMethod
 @WebResult(name = "orders")
 public List<PurchaseOrder> getOrders(){
-	
+	createPurchaseOrder();	
 	return new ArrayList<PurchaseOrder> (purchaseOrders.values());
 }
 
@@ -62,9 +68,62 @@ public void removeOrder(@WebParam(name = "id")Long id) {
 
 
 public static void main(String[] args) {
+	
 	Endpoint.publish("http://localhost:8080/po", new PurchaseOrderWebService());
 	System.out.println("Running .......");
 	
 	
+}
+
+
+private void createPurchaseOrder(){			
+	
+	PurchaseOrder po = new PurchaseOrder();
+	
+	Item item1 = new Item();
+	
+	item1.setProductName("Pen");
+	item1.setQuantity(5);
+	item1.setPrice(new BigDecimal("23.5"));
+	item1.setComment("Ball point pen");
+	
+	Item item2 = new Item();
+	
+	item2.setProductName("Pencil");
+	item2.setQuantity(4);
+	item2.setPrice(new BigDecimal("12.5"));
+	item2.setComment("Natraj pencil");
+	
+	List<Item> items=  new ArrayList<>();
+	items.add(item1);
+	items.add(item2);	
+	
+	po.setItems(items);
+	
+	Address address = new Address();
+	address.setStreet("Madinaaguda");
+	address.setCity("Hyd");
+	address.setCountry(null);
+	address.setPostalCode("500049");
+	
+	Customer customer = new Customer();
+	customer.setName("Murali");
+	customer.setLoyalty(Loyalty.GOLD);
+	customer.setBillingAddress(address);
+	customer.setShippingAddress(address);
+	
+	po.setCustomer(customer);
+	
+//	XMLGregorianCalendar calender= DatatypeFactory.newInstance().newXMLGregorianCalendar(new GregorianCalendar());		
+//	System.out.println("calender :"+ calender);
+	po.setOrderDate(null);
+	
+	po.setComment("this is PO comment");
+	
+	long id = ID_GENERATOR.incrementAndGet();
+	purchaseOrders.put(id, po);
+	
+	System.out.println("po id's : ");
+	System.out.println(purchaseOrders.keySet());
 }
 }
